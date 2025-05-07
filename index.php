223e20da8002385135aea7b10e144073ea8b9713
manuelpezzi <pezzimanuel@gmail.com>
2025-05-07 13:15:51 +0200

    Corretto il valore minimo per la lunghezza della password a 5 caratteri nel modulo di generazione

--- index.php
+++ index.php
@@ -24,8 +24,8 @@ if ($risultato !== null) {
     <div style="text-align: center; margin-top: 50px;">
         <h1>Generatore di Password Sicure</h1>
         <form method="GET" action="index.php">
-            <label for="lunghezza">Lunghezza della password (minimo 6):</label>
-            <input type="number" id="lunghezza" name="lunghezza" min="6" required>
+            <label for="lunghezza">Lunghezza della password (minimo 5):</label>
+            <input type="number" id="lunghezza" name="lunghezza" min="5" max ='20' required>
             <button type="submit">Genera Password</button>
         </form>
     </div>
